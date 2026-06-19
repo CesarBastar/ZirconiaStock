679303e094ada3cf4c8a0b5186f6aa3c0b16ee72
Cesar Antonio Bastar Jimenez <a24214878@alumnos.uady.mx>
2026-06-19 08:12:42 -0600

    Agregar archivos de proyecto.

---
+++ ZirconiaStock/DiscoZirconia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZirconiaStock
+{
+    public class DiscoZirconia
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+
+        public int Grosor { get; set; }
+        public string Tipo { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public DiscoZirconia(int id, string color, int grosor, string tipo, int cantidad)
+        {
+            Id = id;
+
+            Color = color;
+
+            Grosor = grosor;
+
+            Tipo = tipo;
+
+            Cantidad = cantidad;
+        }
+
+    }
+
+}
