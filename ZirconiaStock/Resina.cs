d5e05bc807aacd68b4bf1d6cf74608571c6c264e
Cesar Antonio Bastar Jimenez <a24214878@alumnos.uady.mx>
2026-06-19 14:07:34 -0600

    Agrege la clase resina para tener 2 productos

---
+++ ZirconiaStock/Resina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZirconiaStock
+{
+    public class Resina : Producto
+    {
+        public Resina() { }
+
+        public Resina(int id, string nombre, int cantidad, int stockMinimo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Cantidad = cantidad;
+            StockMinimo = stockMinimo;
+        }
+    }
+
+}
